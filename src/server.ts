1dcd4be72356fd7d72dfb76dc6b07895b0cc6c6e
33357 <im33357dr@gmail.com>
2023-04-30 13:47:03 +0800

    0.0.1

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,21 @@ import { port, config } from '../config';
 import { bigToString, log, getSvg, sleep, Request } from './libs';
 import {
   XenBoxClient,
+  XenBox2Client,
   XenClient,
   XenBoxHelperClient,
   DeploymentInfo
-} from 'xenbox-sdk';
-import { providers } from 'ethers';
+} from 'xenbox-contract-sdk';
+import { BigNumber, providers } from 'ethers';
 
 const provider = new providers.JsonRpcProvider(config[1].provider);
 const xenBox = new XenBoxClient(
   provider,
   DeploymentInfo[1]['XenBox'].proxyAddress
+);
+const xenBox2 = new XenBox2Client(
+  provider,
+  DeploymentInfo[1]['XenBox2'].proxyAddress
 );
 const xenBoxHelper = new XenBoxHelperClient(
   provider,
@@ -21,9 +26,9 @@ const xenBoxHelper = new XenBoxHelperClient(
 );
 const xen = new XenClient(provider);
 const request = new Request();
-
 const app = express();
 const httpServer = http.createServer(app);
+
 const tokenMap: {
   [tokenId: number]: {
     name: string;
@@ -33,7 +38,15 @@ const tokenMap: {
     attributes: any[];
   };
 } = {};
-
+const token2Map: {
+  [tokenId: number]: {
+    name: string;
+    description: string;
+    image: string;
+    lastTime: number;
+    attributes: any[];
+  };
+} = {};
 const rankMap: {
   [day: number]: {
     rank: number;
@@ -93,6 +106,63 @@ app.get('/api/token/*', async function (req, res) {
   }
 });
 
+app.get('/api/token2/*', async function (req, res) {
+  try {
+    const tokenId = Number(req.path.replace('/api/token2/', ''));
+    if (
+      !token2Map[tokenId] ||
+      new Date().getTime() - token2Map[tokenId].lastTime > 60 * 60 * 1000
+    ) {
+      const [token, fee100, fee50, fee20, fee10] = await Promise.all([
+        xenBox2.tokenMap(tokenId),
+        xenBox2.fee100(),
+        xenBox2.fee50(),
+        xenBox2.fee20(),
+        xenBox2.fee10()
+      ]);
+      const proxy = await xenBox2.getProxyAddress(BigNumber.from(token.start));
+      const [mint, userMints] = await Promise.all([
+        xenBoxHelper.calculateMintReward(proxy),
+        xen.userMints(proxy)
+      ]);
+      const amount = token.end - token.start;
+      let fee;
+      if (amount == 100) {
+        fee = fee100;
+      } else if (amount == 50) {
+        fee = fee50;
+      } else if (amount == 20) {
+        fee = fee20;
+      } else {
+        fee = fee10;
+      }
+      const mints = bigToString(
+        mint
+          .mul(amount)
+          .mul(10000 - fee.toNumber())
+          .div(10000),
+        18
+      ).split('.')[0];
+      const time = new Date(userMints.maturityTs.toNumber() * 1000);
+      token2Map[tokenId] = {
+        name: `XenBox2 ${amount}`,
+        description: `${amount} xen account in this box`,
+        lastTime: new Date().getTime(),
+        image: getSvg(amount, mints, time),
+        attributes: [
+          {
+            trait_type: 'Account',
+            value: amount
+          }
+        ]
+      };
+    }
+    res.send(token2Map[tokenId]);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
 app.get('/api/rank/*', async function (req, res) {
   try {
     const day = Number(req.path.replace('/api/rank/', ''));
@@ -128,6 +198,12 @@ async function run() {
     const totalToken = (await xenBox.totalToken()).toNumber();
     for (let i = 0; i < totalToken; i++) {
       await request.update(xenBox.address(), i);
+      await sleep(100);
+    }
+
+    const totalToken2 = (await xenBox2.totalToken()).toNumber();
+    for (let i = 0; i < totalToken; i++) {
+      await request.update(xenBox2.address(), i);
       await sleep(100);
     }
     log(`run end`);
